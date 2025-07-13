03c723afc5122983bf33926b6f12839265a3fb07
Gowtham <nagamallagowtham@gmail.com>
2025-07-13 12:06:51 +0530

    episode-05

--- README.md
+++ README.md
@@ -36,3 +36,19 @@
  *  - Address
  *  - Contact
  */
+
+# Two types of Export/Import
+
+- Default Export/Import
+
+export default Component;
+import Component from "path";
+
+- Named Export/Import
+
+export const Component;
+import {Component} from "path";
+
+
+# React Hooks
+(Normal JS utility functions)
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,23 @@
 import RestaurantCard from "./RestaurantCard"
 import resList from "../utils/mockData"
+import { useState } from "react"
 
 const Body = () => {
+  const [listOfRes, setListOfRes] = useState(resList)
+  const handleFilter = () => {
+    const filteredList = listOfRes.filter(res => {
+        return res.data.avgRating > 4
+    })
+    setListOfRes(filteredList);
+  }
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn">Top Rated Restaurants</button>
+        <button className="filter-btn" onClick={handleFilter}>Top Rated Restaurants</button>
       </div>
       <div className="res-container">
         {
-            resList.map((restaurant) => (
+            listOfRes.map((restaurant) => (
                 <RestaurantCard key={restaurant.data.id} resData={restaurant} />
             ))
         }
